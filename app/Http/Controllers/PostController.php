cb171fffd4a8f360fa5dd6b1753e666bedec81fe
Emmanuel Hernandez <e805177@gmail.com>
2023-11-06 13:04:49 -0600

    Feat: now you can create post and see in your profile

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,8 +15,10 @@ class PostController extends Controller
     // }
     public function index(User $user)
     {
+        $posts  = Post::where('user_id', $user->id)->orderBy('created_at', 'desc')->get();
         return view('layout.dashboard', [
-            'user' => $user
+            'user' => $user,
+            'posts'=>$posts
         ]);
     }
     public function store(Request $request)
