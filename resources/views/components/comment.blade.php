17083b6814c137fc8e03fea385d3a5ae249344c5
Emmanuel Hernandez <e805177@gmail.com>
2024-01-04 23:12:38 -0600

    Fix: changes in view comments

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -1,7 +1,7 @@
-<div class="text-black p-4 antialiased flex">
+<div class="text-black px-4 py-2 antialiased flex">
     <img class="rounded-full h-8 w-8 mr-2 mt-1 " src="https://picsum.photos/id/1027/200/200">
     <div>
-        <div class="bg-gray-100 rounded-lg px-4 pt-2 pb-2.5">
+        <div class="bg-gray-100 rounded-lg px-3 pt-2 pb-2.5">
             <a href="{{ route('post.index', $comment->user) }}" class="text-gray-600 text-sm font-semibold">{{ $comment->user->username }}</a>
             <div class="text-xs leading-snug md:leading-normal">{{ $comment->comment }}</div>
         </div>
--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -6,7 +6,8 @@
                     src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80">
             </div>
             <div class="flex flex-col mb-2 ml-4 mt-1">
-                <a href="{{ route('post.index', $post->user->username) }}" class="text-gray-600 text-sm font-semibold">{{ $post->user->username }}</a>
+                <a href="{{ route('post.index', $post->user->username) }}"
+                    class="text-gray-600 text-sm font-semibold">{{ $post->user->username }}</a>
                 <div class="flex w-full mt-1">
                     <div class="text-blue-700 font-base text-xs mr-1 cursor-pointer">
                         SEO
@@ -43,8 +44,8 @@
         <div class="flex w-full mt-1 pt-2 pl-5">
             <span
                 class="bg-white transition ease-out duration-300 hover:text-red-500 border w-8 h-8 px-2 pt-2 text-center rounded-full text-gray-400 cursor-pointer mr-2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="14px"
-                    viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="14px" viewBox="0 0 24 24"
+                    stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"></path>
                 </svg>
@@ -65,8 +66,8 @@
         <div class="flex justify-end w-full mt-1 pt-2 pr-5">
             <a href="{{ route('post.show', ['post' => $post, 'user' => $post->user]) }}"
                 class="transition ease-out duration-300 hover:bg-blue-50 bg-blue-100 w-8 h-8 px-2 py-2 text-center rounded-full text-blue-400 cursor-pointer mr-2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="14px"
-                    viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="14px" viewBox="0 0 24 24"
+                    stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z">
                     </path>
@@ -74,8 +75,8 @@
             </a>
             <span
                 class="transition ease-out duration-300 hover:bg-gray-50 bg-gray-100 h-8 px-2 py-2 text-center rounded-full text-gray-100 cursor-pointer">
-                <svg class="h-4 w-4 text-red-500" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor" stroke-width="2">
+                <svg class="h-4 w-4 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                    stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z">
                     </path>
