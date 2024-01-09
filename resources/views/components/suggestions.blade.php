d92571f5d9611e4e73b57f8c7dd88b7789cc3b5b
Emmanuel Hernandez <e805177@gmail.com>
2024-01-09 17:01:21 -0600

    Update suggestions.blade.php

--- resources/views/components/suggestions.blade.php
+++ resources/views/components/suggestions.blade.php
@@ -14,13 +14,30 @@ $users = User::get();
                     <img class="w-10 rounded-full" src="{{ asset('profiles/' . $user->image) }}">
                 </a>
                 <div>
-                    <a class="text-xs block bg-white p-1 rounded-full" href="{{ route('post.index', $user->username) }}">
+                    <a class="text-xs font-bold block bg-white p-1 rounded-full" href="{{ route('post.index', $user->username) }}">
                         {{ $user->username }}
                     </a>
                     <p class="text-xs text-gray-400">
                         Suggested for you
-                    </span>
+                        </span>
                 </div>
+                @if ($user->following(auth()->user()))
+                    <form method="POST" action="{{ route('users.unfollow', $user) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="text-red-500 text-xs ml-2">
+                            Unfollow
+                        </button>
+                    </form>
+                @else
+                    <form method="POST" action="{{ route('users.follow', $user) }}">
+                        @csrf
+                        <button type="submit" href=""
+                            class="text-blue-500 text-xs ml-2">
+                            Follow
+                        </button>
+                    </form>
+                @endif
             </li>
         @endforeach
     </ul>
